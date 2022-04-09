5873733c0546f81b29269deba2c0faed3ded9f86
Leon Seipp <l.seipp@microeinhundert.com>
2022-04-09 10:27:31 +0200

    Error message

--- packages/radonis-server/providers/RadonisProvider/hooks/useAdonis.ts
+++ packages/radonis-server/providers/RadonisProvider/hooks/useAdonis.ts
@@ -6,7 +6,9 @@ export const useAdonis = () => {
   const context = useContext(adonisContext);
 
   if (!context) {
-    throw new Error('Cannot use Radonis server hooks on the client');
+    throw new Error(
+      'Cannot use hooks from the "radonis-server" package on the client. Please make sure you only use hooks from the main "radonis" package on the client'
+    );
   }
 
   return context;
