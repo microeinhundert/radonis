1f89ffaedf69dd6b359d0edf8373a2fc7b3cd6e1
Leon Seipp <l.seipp@microeinhundert.com>
2022-04-11 10:54:28 +0200

    Fixes

--- packages/radonis-server/providers/RadonisProvider/components/HydrationRoot.tsx
+++ packages/radonis-server/providers/RadonisProvider/components/HydrationRoot.tsx
@@ -1,9 +1,9 @@
 import type { HydrationRootProps } from '@ioc:Radonis';
+// @ts-ignore No idea why this import fails in GitHub Actions
+import { HydrationContextProvider, useHydration } from '@microeinhundert/radonis';
 import type { FunctionComponent } from 'react';
 import React, { useId } from 'react';
 
-// @ts-ignore No idea why this import fails in GitHub Actions
-import { HydrationContextProvider, useHydration } from '../../../../radonis';
 import { useManifestBuilder } from '../internal/hooks/useManifestBuilder';
 
 export const HydrationRoot: FunctionComponent<HydrationRootProps> = ({
