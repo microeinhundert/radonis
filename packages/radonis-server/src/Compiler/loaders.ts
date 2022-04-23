363a84576ff9e16c00884beb2c16777bbb15f20b
Leon Seipp <l.seipp@microeinhundert.com>
2022-04-23 18:47:56 +0200

    V1

--- packages/radonis-server/src/Compiler/loaders.ts
+++ packages/radonis-server/src/Compiler/loaders.ts
@@ -2,34 +2,14 @@ import type { Loader } from 'esbuild'
 import { extname } from 'path'
 
 export const loaders: { [ext: string]: Loader } = {
-  '.aac': 'file',
-  '.eot': 'file',
-  '.flac': 'file',
-  '.gif': 'file',
-  '.ico': 'file',
-  '.jpeg': 'file',
-  '.jpg': 'file',
-  '.js': 'jsx',
+  '.js': 'js',
   '.jsx': 'jsx',
-  '.json': 'json',
-  '.mp3': 'file',
-  '.mp4': 'file',
-  '.ogg': 'file',
-  '.otf': 'file',
-  '.png': 'file',
-  '.svg': 'file',
   '.ts': 'ts',
   '.tsx': 'tsx',
-  '.ttf': 'file',
-  '.wav': 'file',
-  '.webm': 'file',
-  '.webp': 'file',
-  '.woff': 'file',
-  '.woff2': 'file',
 }
 
 export function getLoaderForFile(file: string): Loader {
   const ext = extname(file)
   if (ext in loaders) return loaders[ext]
-  throw new Error(`Cannot get loader for file ${file}`)
+  throw new Error(`Cannot get loader for file at "${file}"`)
 }
