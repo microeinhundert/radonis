b4b49c0afeae4f378e57ee9aefbb657033139b97
Leon Seipp <l.seipp@microeinhundert.com>
2022-09-21 18:31:25 +0200

    Missing comment

--- example/app/Controllers/Http/SettingsController.ts
+++ example/app/Controllers/Http/SettingsController.ts
@@ -2,6 +2,9 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import { Settings } from 'Views/Settings';
 
 export default class SettingsController {
+  /*
+   * index action
+   */
   public index({ radonis, i18n }: HttpContextContract) {
     return radonis.withTitle(i18n.formatMessage('settings.index.title')).render(Settings);
   }
