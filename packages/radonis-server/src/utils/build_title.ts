23628fc8be3e230afefdf88500224c023bf238a2
Leon Seipp <leon.fm100@gmail.com>
2022-10-21 18:40:00 +0200

    Fixes

--- packages/radonis-server/src/utils/build_title.ts
+++ packages/radonis-server/src/utils/build_title.ts
@@ -10,7 +10,7 @@
 import { separateArray } from '@microeinhundert/radonis-shared'
 
 /**
- * Build the title for the head's <title> tag
+ * Build the value for the head title tag
  */
 export function buildTitle(title: string, prefix: string, suffix: string, separator: string): string {
   return separateArray(
